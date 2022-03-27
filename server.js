1769367a3c5270063ba963cce6ce568139555524
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-27 21:23:46 +0200

    auto number handler

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-const {Schema, model, createConnection} = mongoose;
+const {Schema, model} = mongoose;
 autoIncrement = require('mongoose-auto-increment');
 const bodyParser = require('body-parser');
 
@@ -37,8 +37,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // database
-const connection = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-autoIncrement.initialize(connection);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+/* const connection = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+autoIncrement.initialize(connection); */
 
 const urlSchema = new Schema({
   original_url: {
@@ -51,13 +52,64 @@ const urlSchema = new Schema({
   }
 });
 
+const autoNumberSchema = new Schema({
+  autoNumber: {
+    type: Number,
+    required: true
+  }
+});
+
 const urlObject = model('urlObject', urlSchema);
-urlSchema.plugin(autoIncrement.plugin, { model: 'urlObject', field: 'short_url' });
+const autoNumberObject = model('autoNumberObject', autoNumberSchema);
+/* urlSchema.plugin(autoIncrement.plugin, { model: 'urlObject', field: 'short_url' }); */
+
+// initialize auto number
+initializeAutonumber = (done) => {
+  const autoNumber = new autoNumberObject({
+    autoNumber: 0
+  });
+  autoNumber.save((err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  })
+};
+
+updateAutoNumber = async (done) => {
+  let updateValue;
+  const numberObject = await autoNumberObject.findOne();
+  if (!numberObject) {
+    updateValue = await initializeAutonumber().autoNumber;
+  } else {
+    updateValue = numberObject.autoNumber
+  }
+  autoNumberObject.findOneAndUpdate({},{autoNumber: updateValue + 1}, {new: true}, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  })
+};
+
+createAndSaveDocument = async (inputData, done) => {
+  const url = await new urlObject({
+    original_url: inputData.original_url,
+    short_url: await updateAutoNumber()
+  });
+  url.save((err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
+}
+
+generateAutoNumber = () => {
+  // lookup documents and extract max value of short_url
+}
 
 // request handlers
 app
   .post("/api/shorturl", (req, res) => {
-
+    // validate url using dns.lookup
+    // create response object using generateAutoNumber for short_url 
+    // save object in database using createAndSaveDocument
+    // send response object 
 })
   .get("api/shorturl", (req, res) => {
 
