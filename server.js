56e9dc360a4409a91e73fa524285f96c91d50b99
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-04-05 17:24:45 +0200

    changed connect method

--- server.js
+++ server.js
@@ -38,7 +38,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // database
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  process.env.MONGO_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true }, 
+  () => {console.log('Connection to DB successful')}, 
+  e => console.error(e)
+);
 
 const urlSchema = new Schema({
   original_url: {
