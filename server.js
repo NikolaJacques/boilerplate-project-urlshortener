7430607a7bc84cf8973514dd92b9714703e548ed
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-04-05 18:52:54 +0200

    code cleanup

--- server.js
+++ server.js
@@ -10,20 +10,11 @@ const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
@@ -31,13 +22,12 @@ app.listen(port, function() {
 // solution
 
 // parser middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // database
+
 mongoose.connect(
   process.env.MONGO_URI, 
   { useNewUrlParser: true, useUnifiedTopology: true }, 
@@ -73,6 +63,7 @@ const createAndSaveDocument = async (urlString) => {
 }
 
 // request handlers
+
 app
   .post("/api/shorturl", (req, res) => {
       try {
